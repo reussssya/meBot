0eb430dacbcf23393931903df678f6847de99046
reussssya <everywhereshego@gmail.com>
2022-12-09 17:47:12 +0300

    added some advantages

--- integrations/meBot.cpp
+++ integrations/meBot.cpp
@@ -15,6 +15,23 @@ void Cmebot::writeWordDatabase(std::string word)
 {
     // ...
 }
+void Cmebot::regexHandle()
+{
+    cout << "RegexHandle\n";
+    std::regex reg("([\\w\\s]{0,1024})" "(:)" "([\\w\\s]{0,1024})");
+    std::cmatch cm;
+
+    std::string buffer = "hello : world";
+    // parse from db.meb and put in std::string buffer;
+
+    cout << "started\n";
+    if(std::regex_match(buffer.c_str(), cm, reg))
+    {
+        cout << "in";
+        cout << cm[1];
+    }
+    // ... CONTINUE FAST
+}
 
 void Cmebot::initWordDatabase()
 {
@@ -25,30 +42,14 @@ void Cmebot::initWordDatabase()
     }*/
     receive->open("db.meb");
 
-    #ifdef DEBUG_MODE
+    /*#ifdef DEBUG_MODE
     std::string logbuffer;
     if(receive->is_open()) *receive >> logbuffer;
     else logbuffer += "Can't open database!";
     cout << logbuffer << endl;
-    #endif DEBUG_MODE
-
+    #endif DEBUG_MODE*/
 }
 
-void Cmebot::regexHandle()
-{
-    std::regex reg("([\\w\\s]{0,1024})" "(:)" "([\\w\\s]{0,1024})");
-    std::cmatch cm;
-
-    std::string buffer;
-    // parse from db.meb and put in std::string buffer;
-
-
-    if(std::regex_match(buffer.c_str(), cm, reg))
-    {
-        // for loop for pieces of db.meb's strings;
-    }
-    // ... CONTINUE FAST
-}
 
 void Cmebot::onMessageProcessing(std::string msg)
 {
--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include "integrations/meBot.h"
 
 
-int main(int argc, char** argv)
+int main()
 {
     setlocale(LC_ALL, ".1251");
-    std::string buffer;
-    Cmebot mebot;
+    std::string buffer = "hello : world";
+    Cmebot mebot();
 
-    std::ifstream receive("db.meb");
+    std::regex reg("([\\w\\s]{0,1024})" "(:)" "([\\w\\s]{0,1024})");
+    std::cmatch cm;
+
+    // parse from db.meb and put in std::string buffer;
+
+    if(std::regex_match(buffer.c_str(), cm, reg))
+    {
+        cout << cm[1];
+    }
+    /*std::ifstream receive("db.meb");
+
+
     if(receive.is_open())
     {
         cout << "opened";
         receive >> buffer;
         cout << buffer;
-    }
+    }*/
     
 
     /*while(true)
